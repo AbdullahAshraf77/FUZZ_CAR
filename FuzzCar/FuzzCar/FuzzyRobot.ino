166b472abb69012fc9c386040fd8cf19743bd346
Abdullah Ashraf Farouk <121633182+AbdullahAsh77@users.noreply.github.com>
2024-05-10 03:18:59 +0300

    added some files

---
+++ FuzzCar/FuzzCar/FuzzyRobot.ino
@@ -0,0 +1,18 @@
+/* Title: FuzzCar
+ * Brief: Using the nano board to interface with a robot that will assist doctors 
+ *        in medical labs with organizing the blood samples in a simple and efficient way 
+ * date : 9/5/2024
+ * USERS: Mohammed Ashraf
+ *        Nour Mamdouh
+ *        Abdullah Ashraf
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
